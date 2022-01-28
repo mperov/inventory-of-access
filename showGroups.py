34b8a150281075845b38a762ddca668e4638ebda
Maksim Perov <m.perov@yadro.ru>
2022-01-28 14:36:06 +0300

    function getUser

--- showGroups.py
+++ showGroups.py
@@ -3,6 +3,17 @@
 
 TARGET_GROUPS = [ ]
 
+def getUsers():
+    users = []
+    with open("/etc/passwd" , "r") as f:
+        for line in f.readlines():
+            splited = line.split(":")
+            shell = splited[-1:][0].split('\n')[0].split('/')[-1:][0]
+            homePath = splited[-2:][0].split('/')
+            if 'home' in homePath and shell in ['bash', 'sh']:
+                users.append(splited[0])
+    return users
+
 def getGroups():
     groups = []
     with open("/etc/group" , "r") as f:
