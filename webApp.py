298acc9853c1c5aad941231b429b26401eccd96e
Maksim Perov <coder@frtk.ru>
2024-05-25 22:31:42 +0300

    add customization generating users playbook

--- webApp.py
+++ webApp.py
@@ -53,9 +53,28 @@ def sendGroups():
 def sendUsers():
     if DEBUG:
         print(inspect.currentframe().f_code.co_name)
-    groups = getGroups();
-    users = getUsers();
-    playbook = getUsersPlayBook(users, groups)
+    info = request.get_json(force=True)
+    ig = [] # groups should be included
+    eg = [] # groups should be excluded
+    eu = [] # users should be excluded
+    iu = [] # users should be included
+    arguments = {'expires' : 0, 'create_home' : 'no'}
+    try:
+        if 'ig' in info:
+            ig = info['ig']
+        if 'eg' in info:
+            eg = info['eg']
+        if 'eu' in info:
+            eu = info['eu']
+        if 'iu' in info:
+            iu = info['iu']
+    except:
+        text = "Incorrect json data filling!"
+        return jsonify(result=text)
+
+    groups = getGroups(excluded = eg, included = ig, excludedUsers = eu);
+    users = getUsers(excluded = eu, included = iu);
+    playbook = getUsersPlayBook(users, groups, arguments, included = iu)
     writePlayBook(playbook, "users_playbook.yml")
     return send_from_directory(app.root_path, "users_playbook.yml")
 
